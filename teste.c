0cdb1d03a8bef1e9c846f2e2cbe3d4138d81d61d
luizedsilva <luizedsilva@gmail.com>
2019-04-20 22:01:35 -0300

    Adicionou um comando puts

--- teste.c
+++ teste.c
@@ -1,9 +1,9 @@
 // Acrescentado um comentario
 #include <stdio.h>
 
-
 int main (void) {
   int i;
+  puts ("uma tabela de quadrados");
   for (i = 0; i < 10; i++) {
     printf ("%d x %d = %d\n", i, i, i*i);
   }
